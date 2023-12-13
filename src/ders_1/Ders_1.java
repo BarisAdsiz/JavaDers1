6f478253cca222308a226934d8bcdd3421f24ee3
Barış Adsız <123736772+BarisAdsiz@users.noreply.github.com>
2023-12-13 14:17:05 +0300

    Add files via upload

---
+++ src/ders_1/Ders_1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ders_1;
+
+/**
+ *
+ * @author Barış Adsız
+ */
+public class Ders_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Merhaba Baris Adsiz");
+        System.out.println("Baris \nAdsiz");
+        
+        System.out.println("Merhaba Baris Adsiz \nBaris \nAdsiz");
+    }
+    
+}
